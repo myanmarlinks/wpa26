5c3d5c39865d3463c767a36e0ccc8a1f178268ef
phpcrazy <phpcrazy@gmail.com>
2017-03-19 18:37:19 +0630

    Override

--- oophp/index.php
+++ oophp/index.php
@@ -12,14 +12,12 @@ abstract class Animal {
 		echo "Sleep <br />";
 	}
 }
-
 interface DogInterface {
 	public function bark();
 }
 interface EatInterface {
 	public function eat();
 }
-
 trait Foo {
 	public $bar = "Google"; 
 	public function test() {
@@ -28,8 +26,6 @@ trait Foo {
 }
 
 class Dog extends Animal implements DogInterface, EatInterface {
-
-	
 	public $color;
 	public $leg;	
 	public function __construct($name, $color, $leg = 4) {
@@ -42,33 +38,33 @@ class Dog extends Animal implements DogInterface, EatInterface {
 	}
 	// Scalar Type Declaration (...$thing)
 	// Return Type Declaration :String
-	
+
 	public function bark() {
 		echo "BARK! <br />";
 	}
+
 	public function __destruct() {
 		echo "DOG CLASS DESTROY! <br />";
 	}
 }
-
 class Cat extends Animal {
-
 	use Foo;
-	
 	public function __construct($name)
 	{
 		parent::__construct($name);
 	}
-	
 	public function meow() {
 		echo "Meow! <br />";
 	}
 
+	// method override
+	public function sleep() {
+		echo "Sound Sleep! <br />";
+	}
 	public function __destruct() {
 		echo "CAT CLASS DESTROY! <br />";
 	}
 }
-
 $dog = new Dog("Aung Net", "red", 3);
 $dog->sleep();
 $dog->eat("Meat", "Rice", "Chicken", 5, "Fish", 45);
@@ -76,5 +72,4 @@ $cat = new Cat("Mee Mee");
 $cat->sleep();
 $cat->test();
 echo $cat->bar;
-
 ?>
