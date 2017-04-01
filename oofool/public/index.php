1d315808abe5d0a50b6fac8905ca1243bff7dc6e
phpcrazy <phpcrazy@gmail.com>
2017-04-01 17:36:49 +0630

    SQL

--- oofool/public/index.php
+++ oofool/public/index.php
@@ -1,6 +1,7 @@
 <?php 
 $students = DB::table("stocks")->get();
 class DB extends PDO {
+	private $_table_name;
 
 	public function __construct() {
 		$dsn = 'mysql:dbname=wpa26;host=127.0.0.1';
@@ -8,18 +9,18 @@ class DB extends PDO {
 		$password = '';
 		parent::__construct($dsn, $user, $password);
 	}
-
 	private static $_instance;
-
-	public static function table(  $table_name) {
+	public static function table(string $table_name) {
 		if(!self::$_instance instanceof DB) {
 			self::$_instance = new DB();
 		}
+		self::$_instance->_table_name = $table_name;
 		return self::$_instance;
 	}
 
 	public function get() {
-		echo "STUDENTS";
+		$sql = "SELECT * FROM " . $this->_table_name;
+		var_dump($sql);
 	}
 }
 
