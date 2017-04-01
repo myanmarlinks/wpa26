4deff698486de889e6538326f3d482d0d6529d10
phpcrazy <phpcrazy@gmail.com>
2017-04-01 18:10:25 +0630

    SQL Query

--- oofool/public/index.php
+++ oofool/public/index.php
@@ -1,29 +1,40 @@
 <?php 
-$students = DB::table("stocks")->get();
-class DB extends PDO {
-	private $_table_name;
+$stocks = DB::table("stocks")->get();
+$categories = DB::table("categories")->get();
+var_dump($stocks);
+var_dump($categories);
 
-	public function __construct() {
-		$dsn = 'mysql:dbname=wpa26;host=127.0.0.1';
-		$user = 'root';
-		$password = '';
-		parent::__construct($dsn, $user, $password);
-	}
-	private static $_instance;
-	public static function table(string $table_name) {
-		if(!self::$_instance instanceof DB) {
-			self::$_instance = new DB();
-		}
-		self::$_instance->_table_name = $table_name;
-		return self::$_instance;
-	}
+class DB extends PDO
+{
+    private $_table_name;
 
-	public function get() {
-		$sql = "SELECT * FROM " . $this->_table_name;
-		var_dump($sql);
-	}
+    public function __construct()
+    {
+        var_dump("DB Object Created!");
+        $dsn = 'mysql:dbname=wpa26;host=127.0.0.1';
+        $user = 'root';
+        $password = '';
+        parent::__construct($dsn, $user, $password);
+    }
+
+    public function __destruct()
+    {
+        var_dump("DB Object Destroyed!");
+    }
+    private static $_instance;
+    public static function table(string $table_name)
+    {
+        if (!self::$_instance instanceof DB) {
+            self::$_instance = new DB();
+        }
+        self::$_instance->_table_name = $table_name;
+        return self::$_instance;
+    }
+
+    public function get()
+    {
+        $sql = "SELECT * FROM " . $this->_table_name;
+        $result = $this->query($sql);
+        return $result->fetchAll();
+    }
 }
-
-
-
- ?>
